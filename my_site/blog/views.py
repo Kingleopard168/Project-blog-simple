14cb8ca356be37556ff36e4b18bf59cb33080c00
Toungchai Sriangthong <114647008+Kingleopard168@users.noreply.github.com>
2022-10-09 14:19:57 +0700

    How to dummy Data and connect other template
    How to dummy Data and connect other template

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,78 @@
+from xml.dom.minidom import Identified
 from django.shortcuts import render
+
+from datetime import date
+
+all_posts = [
+    {
+         "slug":"the-Mountains",
+         "image":"mountains.png",
+         "author":"MAXIMILIAN",
+         "date": date(2021,7,21),
+         "title":"Mountain Hiking",
+         "excerpt":"There's nothing like the views you get when hiking in the Mountain And I wasn't even prepared for  what happened whilst I was enjoying the view",
+         "content":""" 
+         Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+            Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+            Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+         """
+    },
+    {
+         "slug":"the-MountainsB",
+         "image":"mountains.png",
+         "author":"MAXIMILIAN",
+         "date": date(2021,7,21),
+         "title":"Mountain HikingB",
+         "excerpt":"There's nothing like the views you get when hiking in the Mountain And I wasn't even prepared for  what happened whilst I was enjoying the view",
+         "content":""" 
+         Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+            Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+            Lorem ipusm dolor amet consectetor adipisicing elit.Officiis nobis
+            aperiam est prasentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero cula ad mollitia? quis architecto ipsam nemo. Odio.
+
+         """
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts= sorted(all_posts,key=get_date)
+    latest_posts= sorted_posts[-3:]
+    return render(request, "blog/index.html",{
+        "posts":latest_posts
+    }
+    
+    )
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html" , {
+        "all_posts": all_posts
+    })
+    
+    
 
 def post_datail(request, slug):
-    return render(request, "blog/post-detail.html")
+    Identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post":Identified_post
+    })
